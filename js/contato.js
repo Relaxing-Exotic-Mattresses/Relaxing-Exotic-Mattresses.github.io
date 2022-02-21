7bbe34a68fc9ddd2083100438a8075f2d2bf85f1
Caio Pinheiro <college13579@gmail.com>
2022-02-20 22:32:45 -0300

    corrige problemas de horario e cor no script

--- js/contato.js
+++ js/contato.js
@@ -1,29 +1,52 @@
-function cores() {
-    let hora = ((new Date() % 86400000) / 3600000) - 3;
-    let corpo = document.querySelector("body"), navbar = document.querySelector("header");
+let qS = html => document.querySelector(html);
 
-    let alteraCor = (elemento, cor) => elemento.style.backgroundColor = cor;
+function cores() {
+    let hora = (((new Date() - (3 * 3600000)) % 86400000) / 3600000);
+    let corpo = qS("body");
+
+    let alteraFundo = (elemento, cor) => elemento.style.backgroundColor = cor;
 
     if (hora >= 5 && hora < 7){
-        alteraCor(corpo, "#CEF2F6");
+        alteraFundo(corpo, "#CEF2F6");
     } else if (hora >= 7 && hora < 12) {
-        alteraCor(corpo, "#ACE8F0");
+        alteraFundo(corpo, "#ACE8F0");
     } else if (hora >= 12 && hora < 17) {
-        alteraCor(corpo, "#A7D0F5");
+        alteraFundo(corpo, "#A7D0F5");
     } else if (hora >= 17 && hora < 19) {
-        alteraCor(corpo, "#8AAAE8");
+        alteraFundo(corpo, "#8AAAE8");
     } else if (hora >= 19 && hora < 23) {
-        alteraCor(corpo, "#486EB8");
+        alteraFundo(corpo, "#486EB8");
     } else {
-        alteraCor(corpo, "#091582");
-        alteraCor(navbar, "#000B32");
+        alteraFundo(corpo, "#091582");
+
+        let alteraCor = (elemento, cor) => elemento.style.backgroundColor = cor;
+
+        let mudanca = (sequencia, cor, funcao) => {
+            for (let i = 0; i<sequencia.length; i++){
+                if (sequencia[i].length) {
+                    mudanca(sequencia[i], cor, funcao);
+                } else {
+                    funcao(sequencia[i], cor);
+                }
+            }
+        }
+
+        let azulEscuro = [qS("header"), qS("#nome"), qS("#email"), qS("#mensagem"), document.querySelectorAll("button")];
+        let branco = [qS("#titulo"), qS("h1"), qS("#texto"), document.querySelectorAll("input")];
+
+        mudanca(azulEscuro, "#000B32", alteraFundo);
+        mudanca(branco, "#dadada", alteraFundo);
+        qS("#titulo").style.borderColor = "#afafaf";
+
+        azulEscuro[4][0].style.cursor = "pointer";
+        azulEscuro[4][1].style.cursor = "pointer";
     }
 
 }
 cores();
 
-let envio = document.querySelector("#enviar");
-let modal = document.querySelector("#modal");
+let envio = qS("#enviar");
+let modal = qS("#modal");
 
 let sumir = () => {
     modal.style.animationName = "sumir";
@@ -31,7 +54,7 @@ let sumir = () => {
 }
 
 envio.addEventListener("click", () => {
-    let nome = document.querySelector("#individuo").value;
+    let nome = qS("#individuo").value;
     let nossoProfessorFavorito = 0, tambemConhecidoComo = ["francisco", "paulo", "freitas", "neto"];
 
     let checagem = pauloFreitas => { if (nome.toLowerCase().includes(pauloFreitas)) nossoProfessorFavorito++; }
@@ -46,4 +69,4 @@ envio.addEventListener("click", () => {
     }
 });
 
-document.querySelector("#modal").addEventListener("click", () => sumir());
+qS("#modal").addEventListener("click", () => sumir());
